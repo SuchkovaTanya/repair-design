d12ae99b1b51f6f2b7d2741c43f5e7f41528f41a
Your Name <suchkovatasu@gmail.com>
2020-03-15 21:48:24 +0300

    add fon before dezign

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     const modal = document.querySelector ( '.modal' );
     const modalBtn = document.querySelectorAll('[data-toggle=modal]');
     const closeBtn = document.querySelector ( '.modal__close' );
+    //const closeObyekt = document.querySelector ( '.modal' );
     const swithModal = () => {
         modal.classList.toggle('modal--visible');
     }
@@ -9,7 +10,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
    modalBtn.forEach(element => {
        element.addEventListener('click',swithModal);     
    });
+   
+   //closeObyekt.addEventListener('click',swithModal);
    closeBtn.addEventListener('click',swithModal);
-  
 
 });
