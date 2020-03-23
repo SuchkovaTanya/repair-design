a3f326463a9757ab2f00150f6f910b9cc83f5ac2
Your Name <suchkovatasu@gmail.com>
2020-03-23 20:56:07 +0300

    feat: add footer, validate forms

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@
    closeBtn.addEventListener('click',swithModal);
 
 });*/
-
+//Настройки формы - модальное окно
 $(document).ready(function() {
     var modal=$('.modal'),
         modalBtn=$('[data-toggle=modal]'),
@@ -26,7 +26,7 @@ $(document).ready(function() {
     closeBtn.on('click', function(){
         modal.toggleClass('modal--visible');
     });
-
+    //Настройки слайдера
     var mySwiper = new Swiper ('.swiper-container', {
         loop: true,
         pagination: {
@@ -43,6 +43,40 @@ $(document).ready(function() {
     var bullets=$('.swiper-pagination');
     next.css('left', prev.width()+20+bullets.width()+20);
     bullets.css('left', prev.width()+20);
-
+    //Активация анимации с применением скролинга
     new WOW().init();
+
+    //Валидация формы
+    $('.modal__form').validate({
+        errorClass: "invalid",
+        rules: {
+          // simple rule, converted to {required:true}
+          userName: {
+              required: true,
+              minlength: 2,
+              maxlength: 15
+            },
+          userPhone: "required",
+          // compound rule
+          userEmail: {
+            required: true,
+            email: true
+          }
+        },
+        //Настройка сообщений об ошибках
+        messages: {
+            userName: {
+                required: "Заполните поле",
+                minlength: "Имя должно быть не менее 2 символов",
+                maxlength: "Имя не должно быть больше 15 символов"
+            },
+            userPhone: "Заполните поле",
+            userEmail: {
+              required: "Заполните поле",
+              email: "Введите корректный email"
+            }
+          }
+      });
+//Маска ввода телефона
+$('[type=tel]').mask('+7(000) 000-00-00'), {placeholder: "+7 (___) ___-__-__"};
 })
